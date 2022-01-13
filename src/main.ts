efb8f0523ff44a337a16ca4855ab58ce4e92e015
drumath <2237k.drumath@gmail.com>
2022-01-13 18:57:36 +0900

    :sparkles: Hello豆腐

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
 import './style.scss';
+import * as BABYLON from '@babylonjs/core';
 
-const renderCanvas = <HTMLCanvasElement>document.getElementById('renderCanvas');
+const main = async () => {
+  const renderCanvas = <HTMLCanvasElement>(
+    document.getElementById('renderCanvas')
+  );
 
-if (renderCanvas) {
-  // something...
-}
+  if (renderCanvas) {
+    const engine = new BABYLON.Engine(renderCanvas, true);
+    const scene = new BABYLON.Scene(engine);
+
+    scene.createDefaultCameraOrLight(true, true, true);
+    scene.createDefaultEnvironment();
+
+    const boxSize = 0.2;
+    const box = BABYLON.MeshBuilder.CreateBox('box', { size: boxSize });
+    box.position.addInPlaceFromFloats(0, boxSize / 2.0, 0);
+
+    engine.runRenderLoop(() => {
+      scene.render();
+    });
+  }
+};
+
+main();
